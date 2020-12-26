516ecfe08f0583b1479347a6536d1b5331c2e1ca
Jali <jali@orca-central.de>
2020-12-26 16:55:46 +0100

    Moved mailer configuration to environment

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,13 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'godmother@hackers.org.il'
   layout 'mailer'
-  smtp_settings = {
-    :address              => "email-smtp.eu-central-1.amazonaws.com",
-    :port                 => 587,
-    :domain               => "hackers.org.il",
-    :user_name            => "USERNAME HERE",
-    :password             => "PASSWORDHERE",
-    :authentication       => "plain",
-    :enable_starttls_auto => true
-  }
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,6 +40,17 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.perform_deliveries = true
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'email-smtp.eu-central-1.amazonaws.com',
+    port:                 587,
+    domain:               'hackers.org.il',
+    user_name:            '<username>',
+    password:             '<password>',
+    authentication:       'login',
+    enable_starttls_auto: true 
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
