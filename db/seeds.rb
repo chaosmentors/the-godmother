8e4f6a9f71729a1e43d8af8810eb8233f9d486d9
Oliver Mesieh <olivermesieh@gmail.com>
2024-11-16 20:28:03 +0100

    Add database seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,49 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Person.destroy_all
+Tag.destroy_all
+
+languages = ['german', 'deutsch', 'english', 'englisch', 'spanish', 'español', 'french', 'français']
+
+100.times do
+  person = Person.create(
+    pronoun: ['she/her', 'he/him', 'they/them'].sample,
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    role: Person.role_name_to_value(['mentee', 'mentor'].sample),
+    state: Person.state_name_to_value('waiting'),
+    about: Faker::Lorem.paragraph,
+    validated_at: Time.now
+  )
+
+  tags = []
+  10.times do
+    if rand < 0.5
+      tags << Tag.find_or_create_by(name: languages.sample)
+    else
+      tags << Tag.find_or_create_by(name: Faker::Lorem.word)
+    end
+  end
+  person.tags << tags.sample(rand(1..3))
+
+  person.save!
+end
+
+puts "Created #{Person.count} persons and #{Tag.count} tags"
+
+# Add a godmother
+godmother = Person.create(
+  pronoun: 'she/her',
+  name: 'Godmother',
+  email: 'godmother@example.com',
+  role_name: :godmother,
+  state: Person.state_name_to_value('verified'),
+  about: 'I am the godmother of this project.',
+  validated_at: Time.now,
+  password: 'password',
+  password_confirmation: 'password'
+)
+
+godmother.save(validate: false)
+
+puts "Created godmother with email #{godmother.email} and password 'password'"
