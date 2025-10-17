099da45db3d0ac0f3936fd0fe2ce01700bbb2232
Oliver Mesieh <olivermesieh@gmail.com>
2025-10-17 15:50:19 +0200

    Fix missing group id for mentors assigned to a group

--- app/models/group.rb
+++ app/models/group.rb
@@ -6,6 +6,7 @@ class Group < ApplicationRecord
   validates :label, presence: true, uniqueness: true
 
   before_destroy :remove_group_id_from_persons
+  after_save :update_mentor_group_id
 
   def mentees
     Person.where(group_id: self.id).where(role: Person.role_name_to_value('mentee'))
@@ -29,6 +30,25 @@ class Group < ApplicationRecord
   def remove_group_id_from_persons
     mentees.update_all(group_id: nil)
 
+    if mentor.present?
+      mentor.update_column(:group_id, nil)
+    end
+
     PersonStateAligner.align_state
+  end
+
+  def update_mentor_group_id
+    if saved_change_to_mentor_id?
+      old_mentor_id, new_mentor_id = saved_change_to_mentor_id
+
+      if old_mentor_id.present?
+        old_mentor = Person.find_by(id: old_mentor_id)
+        old_mentor&.update_column(:group_id, nil)
+      end
+
+      if new_mentor_id.present?
+        mentor&.update_column(:group_id, self.id)
+      end
+    end
   end
 end
